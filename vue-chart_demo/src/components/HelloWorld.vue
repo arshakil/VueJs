ed921c023a0ae828d3cfe915b98e0d81b812f623
Azizur <azizur.rahman363410@gmail.com>
2020-09-28 17:59:26 +0600

    added VueChart Bar, Doughnut, line

---
+++ vue-chart_demo/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <h1>Chart Demo</h1>
+<div class="container">
+    <div class="row mt-4">
+      <div class="col-6">
+        <!-- <ChartBar /> -->
+      </div>
+        <div class="col-6">
+        <ChartDoughnut />
+      </div>   
+    </div>
+
+
+    <hr style="background:red">
+    <hr style="background:blue">
+
+
+        <div class="row mt-4">
+      <div class="col-6">
+       <ChartLine />
+      </div>
+        <div class="col-6">
+        <ChartBarSingle />
+      </div>
+    </div>
+  
+</div>
+  </div>
+</template>
+
+<script>
+// import ChartBar from "@/components/chart-bar";
+import ChartDoughnut from "@/components/chart-doughnut";
+import ChartLine from "@/components/chart-line";
+import ChartBarSingle from "@/components/test";
+export default {
+   components: {
+    // ChartBar,
+    ChartDoughnut,
+    ChartLine,
+    ChartBarSingle,
+  }
+
+}
+</script>
+
+<style>
+
+.grid {
+  display: grid;
+  row-gap: 2rem;
+}
+
+</style>
---
+++ vue-chart_demo/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+Vue.config.productionTip = false
+
+// Globally register chart# https://yasminzy.com/vue/chart.html#steps
+import "chart.js";
+import "hchs-vue-charts";
+Vue.use(window.VueCharts);
+
+// Globally register bootstrap-vue components# npm install bootstrap jquery popper.js
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
